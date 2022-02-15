274d1e44711e6c30f92b19141ccb5e125483b33b
unknown <zaladivyb@gmail.com>
2022-02-15 20:35:58 +0530

    created a project with create-react-app about clothing

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Directory from "../../components(reusable)/directory/directory.component";
+import './homecomponent.style.scss'
+export const Homepage = () =>(
+    <div className="homepage">
+       <Directory />
+    </div>
+)
+
