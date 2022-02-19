3f090fe50ff4bf6354d19e5dcbdc4b0afc103b20
unknown <zaladivyb@gmail.com>
2022-02-19 14:28:41 +0530

    updated shopdata with new data

--- src/components(reusable)/preview-collection/preview-collection.component.jsx
+++ src/components(reusable)/preview-collection/preview-collection.component.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 import CollectionItem from "../collection-items/collection-items.component";
 
-import './preview-collection.style.scss';
+import "./preview-collection.style.scss";
 
-const CollectionPreview = ({title,items}) =>(
-    <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <div className="preview">
-            {
-                items
-                .filter((item,idx)=>idx<4)
-                .map(({id,...otherItemProps})=>(
-                    <CollectionItem key={id} {...otherItemProps} />
-                ))
-            }
-        </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-)
+  </div>
+);
 
 export default CollectionPreview;
